ef8479664a73df65245081ec348f3b7bb2691ec2
Rafael <Rafaelblink@gmail.com>
2019-09-06 00:55:52 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'NgInternationalization';
+  title = "NgInternationalization";
+
+  constructor(private translate: TranslateService) {
+    translate.addLangs(["en", "pt"]);
+    translate.setDefaultLang("en");
+  }
+
+  translateEnglish() {
+    this.translate.use("en");
+    console.log("Language changed! :)");
+  }
+
+  translatePortuguese() {
+    this.translate.use("pt");
+    console.log("Idioma modificado! :)");
+  }
 }
